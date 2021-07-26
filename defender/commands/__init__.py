afd2fa813d361aaf7152a5318da0edc10bc46878
Nanako0129 <john990129@gmail.com>
2021-07-26 13:37:11 +0800

    del

--- defender/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-from .manualmodules import ManualModules
-from .settings import Settings
-from .stafftools import StaffTools
-from ..abc import CompositeMetaClass
-
-class Commands(ManualModules, StaffTools, Settings, metaclass=CompositeMetaClass): # type: ignore
-    """Class joining all command subclasses"""
