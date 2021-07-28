c97a0f0c06d030140df8b749c986f602f652c53b
Nanako0129 <john990129@gmail.com>
2021-07-28 14:37:40 +0800

    add db settings of host, pswd, name

--- discordsrv/discordsrv.py
+++ discordsrv/discordsrv.py
@@ -1,4 +1,5 @@
 import asyncio
+from os import name
 import discord
 from redbot.core import commands
 from redbot.core import Config
@@ -16,10 +17,17 @@ class DiscordSRV(commands.Cog):
                 "db_host": None,
                 "db_user": None,
                 "db_pswd": None,
-                "db_table": None
+                "db_name": None
             }
         }
         self.config.register_guild(**default_guild)
+
+    @staticmethod
+    async def delete_quietly(message: discord.Message):
+        try:
+            await message.delete()
+        except discord.HTTPException:
+            pass
 
     async def send_and_query_response(
         self,
@@ -27,7 +35,7 @@ class DiscordSRV(commands.Cog):
         query: str,
         pred: MessagePredicate = None,
         *,
-        timeout: int = 60,
+        timeout: int = 60
     ) -> str:
         if pred is None:
             pred = MessagePredicate.same_context(ctx)
@@ -46,7 +54,7 @@ class DiscordSRV(commands.Cog):
     @commands.group(aliases=["dsrv"])
     async def discordsrv(self, ctx: commands.Context):
         """WIP"""
-        await ctx.send("WIP")
+        await ctx.send("> WIP")
 
     @discordsrv.command(name="linked")
     async def dsrv_linked(self, ctx: commands.Context, member: discord.Member):
@@ -64,20 +72,68 @@ class DiscordSRV(commands.Cog):
     @discordsrv.group(name="set")
     async def dsrv_set(self, ctx):
         """Setting discordSRV database"""
+        pass
 
-    async def dsrv_set_host(self, ctx: commands.Context, hostname: str):
+    @dsrv_set.command(name="host")
+    async def dsrv_set_host(self, ctx: commands.Context):
         """set the database host(e.g. `domain.com`, `123.45.67.8`) of discordSRV"""
         #regex for hostname and ip address: https://regex101.com/r/0WMysi/2
         pred = MessagePredicate.regex(
-            pattern="^(((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|((([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z|[A-Za-z][A-Za-z0-9\‌​-]*[A-Za-z0-9])))$"
+            pattern="^(((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|((([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)+([A-Za-z|[A-Za-z][A-Za-z0-9\\‌​-]*[A-Za-z0-9])))$"
             )
         try:
-            await self.send_and_query_response(
-                ctx, "Please send the host you want to set:(e.g. `domain.com`, `123.45.67.8`)", pred
+            result = await self.send_and_query_response(
+                ctx, "Please send the host you want to set: (e.g. `domain.com`, `123.45.67.8`)", pred
             )
         except asyncio.TimeoutError:
             await ctx.send("Query timed out, nothing changed.")
         
-        if pred.result is not NULL:
-            await self.config.guild(ctx.guild).set_raw("database", "db_host", value=hostname)
-            await ctx.send("Database host has been set to {}.".format(pred.result))
+        if result is not None:
+            await self.config.guild(ctx.guild).set_raw("database", "db_host", value=result)
+            await ctx.send("Database host has been set to {}.".format(result))
+    
+    @dsrv_set.command(name="port")
+    async def dsrv_set_port(self, ctx: commands.Context):
+        """set the database port of DiscordSRV"""
+        pred = MessagePredicate.regex(
+            pattern="^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$"
+        )
+        try:
+            result = await self.send_and_query_response(
+                ctx, "Please send the port you want to set:", pred
+            )
+        except asyncio.TimeoutError:
+            await ctx.send("Query timed out, nothing changed.")
+        
+        if result is not None:
+            await self.config.guild(ctx.guild).set_raw("database", "db_port", value=result)
+            await ctx.send("Database port has been set to {}.".format(result))
+
+    @dsrv_set.command(name="password", aliases=["pswd", "passwd", "ps"])
+    async def dsrv_set_pswd(self, ctx: commands.Context):
+        """set the database password of DiscordSRV"""
+        try:
+            result = await self.send_and_query_response(
+                ctx, "Please set the password (limit in 32 char):"
+            )
+        except asyncio.TimeoutError:
+            await ctx.send("Query timed out, nothing changed.")
+
+        if result is not None:
+            await self.config.guild(ctx.guild).set_raw("database", "db_pswd", value=result)
+            await ctx.author.send("Database password has been set to {}.".format(result))
+            await ctx.tick()
+
+    @dsrv_set.command(name="name")
+    async def dsrv_set_table(self, ctx: commands.Context):
+        """set the database name that DiscordSRV using"""
+        try:
+            result = await self.send_and_query_response(
+                ctx, "Please input the table name (limit in 64 char):"
+            )
+        except asyncio.TimeoutError:
+            await ctx.send("Query timed out, nothing changed.")
+
+        if result is not None:
+            await self.config.guild(ctx.guild).set_raw("database", "db_name", value=result)
+            await ctx.send("DiscordSRV database's name has been set to {}.".format(result))
